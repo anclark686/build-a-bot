29ec3ad7fd6807e6f8da979c4b4b735b7fc94325
Alycia Clark <Alycia.Clark@forsta.com>
2023-05-27 21:26:08 -0400

    initial commit

---
+++ src/count/CountVue.vue
@@ -0,0 +1,20 @@
+<template>
+    Count: {{ count }}
+    <button @click="increment()">Increment Count</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup(props) {
+        let count = ref(0)
+
+        const increment = () => count.value = count.value + 1;
+
+        return {
+            count,
+            increment,
+        }
+    }
+}
+</script>
